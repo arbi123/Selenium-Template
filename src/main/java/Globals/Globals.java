154d8d16ef33d22617771850c45e26d6134678f3
arbi.topi <topiarbi@gmail.com>
2025-03-07 16:19:50 +0100

    first commit getting readt the webdriver

---
+++ src/main/java/Globals/Globals.java
@@ -0,0 +1,10 @@
+package Globals;
+
+public class Globals {
+    public static String browserType = "chrome";
+    public static String fullName = "Arbi Topi";
+
+    public static String validEmail = "test@user";
+
+    public static String validPassword = "Test@123.";
+}
---
+++ src/main/java/Utilities/BaseInformation.java
@@ -0,0 +1,107 @@
+package Utilities;
+
+import Globals.Globals;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseInformation {
+
+    private static WebDriver driver;
+    private static WebDriverWait wait ;
+
+
+    public static BaseInformation getBaseInformation() {
+        return new BaseInformation();
+    }
+
+
+    public static WebDriver getDriver() {
+
+
+        if (driver == null) {
+            String browserType = Globals.browserType.toLowerCase();
+
+            switch (browserType) {
+
+                case "chrome" -> {
+
+
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--remote-allow-origins=*");
+
+                    String downloadFilepath = "C:\\Users\\Arbi.topi\\Downloads";
+                    Map<String, Object> prefs = new HashMap<>();
+                    prefs.put("download.default_directory", downloadFilepath);
+                    prefs.put("download.prompt_for_download", false);
+                    prefs.put("download.directory_upgrade", true);
+                    prefs.put("safebrowsing.enabled", true);
+
+
+                    // Additional security bypasses
+                    options.setExperimentalOption("prefs", prefs);
+                    options.addArguments("--allow-running-insecure-content");
+                    options.addArguments("--unsafely-treat-insecure-origin-as-secure=http://213.32.46.87:6070/");
+                    options.setAcceptInsecureCerts(true);
+                    options.setHeadless(true);
+                    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Arbi.topi\\IdeaProjects\\PSQD\\src\\main\\resources\\chromedriver.exe");
+                    driver = new ChromeDriver(options);
+                    driver.manage().window().maximize();
+                    driver.manage().window().maximize();
+                    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                }
+
+                case "firefox" -> {
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+                    // Set the correct path to the Firefox binary
+                    firefoxOptions.setBinary("C:\\Users\\Arbi.topi\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
+
+                    String downloadFilepath = "C:\\Users\\Arbi.topi\\Downloads";
+                    firefoxOptions.addPreference("browser.download.folderList", 2);
+                    firefoxOptions.addPreference("browser.download.dir", downloadFilepath);
+                    firefoxOptions.addPreference("browser.download.manager.showWhenStarting", false);
+                    firefoxOptions.addPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf, application/octet-stream");
+                    firefoxOptions.setAcceptInsecureCerts(true);
+
+                    // Ensure GeckoDriver is correctly set up
+                    System.setProperty("webdriver.gecko.driver", "C:\\Users\\Arbi.topi\\IdeaProjects\\PSQD\\src\\main\\resources\\geckodriver.exe");
+
+                    driver = new FirefoxDriver(firefoxOptions);
+                    driver.manage().window().maximize();
+                    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                }
+                case "edge" -> {
+                    EdgeOptions options = new EdgeOptions();
+                    Map<String, Object> prefs = new HashMap<String, Object>();
+                    prefs.put("profile.default_content_setting_values.notifications", 2);
+                    prefs.put("credentials_enable_service", false);
+                    prefs.put("profile.password_manager_enabled", false);
+                    prefs.put("autofill.profile_enabled", false);
+                    options.setExperimentalOption("prefs", prefs);
+                    options.addArguments("--disable-extensions");
+                    options.addArguments("--disable-infobars");
+                    options.addArguments("--disable-notifications");
+                    options.addArguments("--remote-allow-origins=*");
+                    System.setProperty("webdriver.edge.driver", "C:\\Users\\Arbi.topi\\IdeaProjects\\PSQD\\src\\main\\resources\\msedgedriver.exe");
+                    driver = new EdgeDriver(options);
+                    driver.manage().window().maximize();
+                    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                }
+                default -> {
+                    System.out.println("Unsupported browser: " + browserType);
+                }
+            }
+        }
+
+        return driver;
+    }}
